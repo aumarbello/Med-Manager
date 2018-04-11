84ac5297d44cf158059fcd732bc091f1dffd0a9f
Ahmed Umar <khattabu@gmail.com>
2018-04-11 11:48:30 +0100

    Updated Detail package

--- app/src/main/java/com/khattabu/med_manager/presentation/add/AddMedicationActivity.java
+++ app/src/main/java/com/khattabu/med_manager/presentation/add/AddMedicationActivity.java
@@ -1,10 +1,54 @@
 package com.khattabu.med_manager.presentation.add;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.widget.TextView;
+
+import com.khattabu.med_manager.R;
 import com.khattabu.med_manager.presentation.base.BaseActivity;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by ahmed on 4/10/18.
  */
 
 public class AddMedicationActivity extends BaseActivity {
+    @Inject
+    AddMedicationRepository repository;
+
+    @BindView(R.id.title)
+    TextInputEditText title;
+
+    @BindView(R.id.desc)
+    TextInputEditText desc;
+
+    @BindView(R.id.start_date)
+    TextView startDate;
+
+    @BindView(R.id.end_date)
+    TextView endDate;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add_medication);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.select_date)
+    public void selectDate(){
+
+    }
+
+    @OnClick(R.id.add_medication)
+    public void addMedication(){
+
+    }
 }
--- app/src/main/java/com/khattabu/med_manager/utils/DateUtils.java
+++ app/src/main/java/com/khattabu/med_manager/utils/DateUtils.java
@@ -17,4 +17,38 @@ public final class DateUtils {
         SimpleDateFormat dateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
         return dateFormat.format(calendar.getTime());
     }
+
+    public static String getInterval(int intervalValue, String intervalType){
+        return String.format(Locale.getDefault(), "%d %s",
+                intervalValue, intervalType);
+    }
+
+    public static String compareDates(long begin, long end){
+        Calendar beginCalender = Calendar.getInstance();
+        beginCalender.setTimeInMillis(begin);
+
+        Calendar endCalender = Calendar.getInstance();
+        endCalender.setTimeInMillis(end);
+
+        if (end < begin){
+            return "--";
+        }
+
+        int beginWeek = beginCalender.get(Calendar.WEEK_OF_YEAR);
+        int endWeek = endCalender.get(Calendar.WEEK_OF_YEAR);
+
+        if (beginWeek - endWeek < 1){
+            int beginDay = beginCalender.get(Calendar.DAY_OF_YEAR);
+            int endDay = endCalender.get(Calendar.DAY_OF_YEAR);
+
+            return getComparedDate(endDay - beginDay, " Days");
+        }
+
+        return getComparedDate(endWeek - beginWeek, " Weeks");
+    }
+
+    private static String getComparedDate(int diff, String factor){
+        String format = "%d %s";
+        return String.format(Locale.getDefault(), format, diff, factor);
+    }
 }
